3444386f90156242a779f96a16a27ae1f4edb50d
abhyrrr <104734000+abhyrrr@users.noreply.github.com>
2026-03-20 17:41:58 -0500

    Add user input sanitation

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,6 +8,13 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
+def request_santized_number(prompt: str) -> float:
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
